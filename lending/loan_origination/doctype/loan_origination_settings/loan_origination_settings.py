07c6c2c5ef561264846bf2f0ac78eede854ea846
Ayush Chaudhari <ayush@frappe.io>
2025-08-07 16:50:45 +0530

    refactor: loan origination settings unique constraints

--- lending/loan_origination/doctype/loan_origination_settings/loan_origination_settings.py
+++ lending/loan_origination/doctype/loan_origination_settings/loan_origination_settings.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
+
 import frappe
 from frappe.model.document import Document
+
+fields_that_can_have_unique_constraints = ["mobile_no", "email_id"]
 
 
 class LoanOriginationSettings(Document):
@@ -20,27 +23,46 @@ class LoanOriginationSettings(Document):
 	pass
 
 	def before_save(self):
-		fields = ["mobile_no", "email_id"]
-		field_string = ", ".join([f"'{i}'" for i in fields])  # for raw sql querying
+		if self.unique_customer:
+			add_unique_constraints()
+		else:
+			remove_unique_constraints()
 
-		fields_with_unique_constraints = frappe.db.sql(
-			f"""
+
+def add_unique_constraints():
+	fields_with_unique_constraints = get_fields_with_unique_constraints()
+
+	# for field in fields_with_unique_constraints
+	for field in set(fields_that_can_have_unique_constraints).difference(
+		set(fields_with_unique_constraints)
+	):
+		try:
+			frappe.db.add_unique("Customer", field)
+
+		except Exception as e:
+			# remove any added constraints
+			remove_unique_constraints()
+
+
+def remove_unique_constraints():
+	fields_with_unique_constraints = get_fields_with_unique_constraints()
+	for field in fields_with_unique_constraints:
+		frappe.db.sql_ddl(f"""alter table tabCustomer drop index unique_{field}""")
+
+
+def get_fields_with_unique_constraints():
+	field_string = ", ".join(
+		[f"'{i}'" for i in fields_that_can_have_unique_constraints]
+	)  # for raw sql querying
+	fields_with_unique_constraints = frappe.db.sql(
+		f"""
 															select column_name
 															from information_schema.statistics
 															where table_name='tabCustomer'
 															and column_name in ({field_string})
 															and non_unique = 0
 														"""
-		)
-		fields_with_unique_constraints = [i[0] for i in fields_with_unique_constraints]
-		if self.unique_customer:
-			# for field in fields_with_unique_constraints
-			for field in set(fields).difference(set(fields_with_unique_constraints)):
-				frappe.db.add_unique("Customer", field)
-		else:
-			remove_unique_constraints(fields_with_unique_constraints)
+	)
+	fields_with_unique_constraints = [i[0] for i in fields_with_unique_constraints]
 
-
-def remove_unique_constraints(fields_with_unique_constraints):
-	for field in fields_with_unique_constraints:
-		frappe.db.sql(f"""alter table tabCustomer drop index unique_{field}""")
+	return fields_with_unique_constraints
