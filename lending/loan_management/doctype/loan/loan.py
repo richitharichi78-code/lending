6b6ee82d101dcad2eea0ab8a1420080eca9c2615
Nihantra Patel <nihantra@frappe.io>
2026-02-18 10:26:21 +0530

    fix: watch period handling for non-NPA restructure and later NPA transition

--- lending/loan_management/doctype/loan/loan.py
+++ lending/loan_management/doctype/loan/loan.py
@@ -932,7 +932,7 @@ def update_days_past_due_in_loans(
 		applicant = loan_details.get("applicant")
 		company = loan_details.get("company")
 		freeze_date = loan_details.get("freeze_date")
-		watch_period_end_date = loan_details.get("watch_period_end_date")
+		existing_watch_period_end_date = loan_details.get("watch_period_end_date")
 
 		if not ignore_freeze and freeze_date and getdate(freeze_date) < getdate(posting_date):
 			return
@@ -955,15 +955,6 @@ def update_days_past_due_in_loans(
 				days_past_due = 0
 				is_npa = 0
 
-			if watch_period_end_date and days_past_due == 1:
-				watch_period_days = frappe.db.get_value(
-					"Company", company, "watch_period_post_loan_restructure_in_days"
-				)
-				watch_period_end_date = add_days(demand.demand_date, watch_period_days)
-				update_watch_period_date_for_all_loans(
-					watch_period_end_date, applicant_type, applicant
-				)
-
 			if posting_date == add_days(getdate(), -1) or force_update_dpd_in_loan:
 				update_loan_and_customer_status(
 					demand.loan,
@@ -977,6 +968,24 @@ def update_days_past_due_in_loans(
 					loan_disbursement=disbursement,
 					is_backdated=is_backdated,
 					dpd_threshold=threshold,
+				)
+
+			watch_period_days = frappe.db.get_value(
+				"Company", company, "watch_period_post_loan_restructure_in_days"
+			)
+
+			watch_period_start_date = (
+				demand.demand_date
+				if existing_watch_period_end_date and days_past_due == 1
+				else getdate(posting_date)
+				if is_npa and not existing_watch_period_end_date
+				else None
+			)
+
+			if watch_period_start_date:
+				watch_period_end_date = add_days(watch_period_start_date, watch_period_days)
+				update_watch_period_date_for_all_loans(
+					watch_period_end_date, applicant_type, applicant
 				)
 
 			create_dpd_record(
